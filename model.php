ed4497d00a252ee864a0cec11eb8d9053f04178b
OliverMasys <noriaoliver@gmail.com>
2025-03-06 21:07:04 -0600

    Fix database connect
    Put the right connect for database.

--- model.php
+++ model.php
@@ -5,12 +5,12 @@ class Model
     public function getData(): array
     {
         $dsn = "mysql:
-        host=localhost;
+        host=sherd_JimBelushi;
         dbname=adv_php;
         charset=utf8;
         port=3306";
 
-        $pdo = new PDO($dsn,"adv_php_user","Secret", [
+        $pdo = new PDO($dsn,"sherd_JimBelushi","adpujdB734kf3", [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
 
